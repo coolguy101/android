97a1d8d7973dfd4f2f7aeec8f6abc6bd3819f530
coolguy101 <jiang_501@hotmail.com>
2014-06-30 14:53:46 -0600

    tabs

--- src/com/example1/jabberchatting/MainActivity.java
+++ src/com/example1/jabberchatting/MainActivity.java
@@ -1,16 +1,38 @@
 package com.example1.jabberchatting;
 
 import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends FragmentActivity {
-
+public class MainActivity extends FragmentActivity implements TabListener {
+	private ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //get action bar
+        actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        //set up listener and text for tabs
+        ActionBar.Tab tab1 = actionBar.newTab();
+        tab1.setTabListener(this);
+        tab1.setText("Frinds");
+        ActionBar.Tab tab2 = actionBar.newTab();
+        tab2.setTabListener(this);
+        tab2.setText("Chats");
+        ActionBar.Tab tab3 = actionBar.newTab();
+        tab3.setTabListener(this);
+        tab3.setText("More");
+        //adding tabs
+        actionBar.addTab(tab1);
+        actionBar.addTab(tab2);
+        actionBar.addTab(tab3);
+        
     }
 
 
@@ -20,5 +42,26 @@ public class MainActivity extends FragmentActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+	@Override
+	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onTabSelected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
     
 }
