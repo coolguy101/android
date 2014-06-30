555725fc3b96506782cb2e53643b595b49cf6746
coolguy101 <jiang_501@hotmail.com>
2014-06-30 15:22:17 -0600

    library added

--- src/com/example1/jabberchatting/MainActivity.java
+++ src/com/example1/jabberchatting/MainActivity.java
@@ -6,22 +6,51 @@ import android.app.ActionBar.Tab;
 import android.app.ActionBar.TabListener;
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.Menu;
+import android.widget.FrameLayout;
 
 public class MainActivity extends FragmentActivity implements TabListener {
 	private ActionBar actionBar;
+	private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new viewPagerAdapter(getSupportFragmentManager()));
+        viewPager.setOnPageChangeListener(new OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int arg0) {
+				// TODO Auto-generated method stub
+				actionBar.setSelectedNavigationItem(arg0);
+			}
+			
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
         //get action bar
         actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         //set up listener and text for tabs
         ActionBar.Tab tab1 = actionBar.newTab();
         tab1.setTabListener(this);
-        tab1.setText("Frinds");
+        tab1.setText("Friends");
         ActionBar.Tab tab2 = actionBar.newTab();
         tab2.setTabListener(this);
         tab2.setText("Chats");
@@ -32,6 +61,8 @@ public class MainActivity extends FragmentActivity implements TabListener {
         actionBar.addTab(tab1);
         actionBar.addTab(tab2);
         actionBar.addTab(tab3);
+        
+        
         
     }
 
@@ -54,13 +85,49 @@ public class MainActivity extends FragmentActivity implements TabListener {
 	@Override
 	public void onTabSelected(Tab arg0, FragmentTransaction arg1) {
 		// TODO Auto-generated method stub
-		
+		viewPager.setCurrentItem(arg0.getPosition());
 	}
 
 
 	@Override
 	public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	class viewPagerAdapter extends FragmentPagerAdapter
+	{
+
+		public viewPagerAdapter(FragmentManager fm) {
+			super(fm);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public Fragment getItem(int arg0) {
+			// TODO Auto-generated method stub
+			Fragment fragment = new BlankFragmenta();
+			if(arg0 == 0)
+			{
+				fragment = new BlankFragmenta();
+			}
+			if(arg0 ==1)
+			{
+				fragment = new BlankFragmentb();
+			}
+			if(arg0 == 2)
+			{
+				fragment = new BlankFragmentc();
+			}
+			
+			return fragment;
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 3;
+		}
 		
 	}
     
